d026d82002fff9a3d5a8e916b14a779eb4c8a5c2
Michael Schmitz <michael@schmitztech.com>
2013-06-26 13:34:45 -0700

    Group extractions without regard to spacing.
    This removes most near-duplicates but has the problem that when clicking
    through to see the sources of the extractions, not all of them will show
    up.

--- webapp/app/models/ExtractionGroup.scala
+++ webapp/app/models/ExtractionGroup.scala
@@ -7,18 +7,18 @@ case class ExtractionGroup(title: String, properScore: Double, instances: List[E
 object ExtractionGroup {
   def from(part: ExtractionPart, instances: List[ExtractionInstance]) = {
     // deduplicate and count instances
-    val deduped = instances.groupBy(inst => (inst.arg1, inst.rel, inst.arg2)).map { case(tuple, list) =>
+    val deduped = instances.groupBy(inst => Iterable(inst.arg1, inst.rel, inst.arg2).mkString(" ").replaceAll("\\s+", "")).map { case (tuple, list) =>
       list.head.copy(count = list.size)
     }
-    
+
     // group instances by the grouping part
     deduped.groupBy(inst => part(inst) map (_.toLowerCase.replaceAll("\\s+", " "))).map { case (key, instances) =>
       val postags = part.postags(instances.head).split(" ")
 
-        val properScore =
-          if (postags.find(_ startsWith "NNP").isDefined) 1
-          else if (postags.find(_ startsWith "PRP").isDefined) -1
-          else 0
+      val properScore =
+        if (postags.find(_ startsWith "NNP").isDefined) 1
+        else if (postags.find(_ startsWith "PRP").isDefined) -1
+        else 0
 
       ExtractionGroup(key.mkString("; "), properScore, instances.toList.sortBy(_.score(part)))
     }.toList.sorted(ExtractionGroupOrdering)
